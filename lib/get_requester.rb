5f27644ddd7b49e3fb66f2bd240d5b9d3eaa6884
ppidcard <tvxqlikei@live.cn>
2021-08-10 10:56:37 +1000

    Done

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,25 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+    attr_accessor :url, :res
+    @res = ''
+    def initialize(url)
+        @url = url
+        self.getData
+    end
+
+    def getData
+        uri = URI.parse(url)
+        @res = Net::HTTP.get_response(uri)
+    end
+
+    def get_response_body
+        @res.body
+    end
+
+    def parse_json
+        JSON.parse(self.get_response_body)
+    end
+end
